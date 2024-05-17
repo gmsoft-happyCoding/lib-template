aa46caa7ac9f99b7e2992b09559df4e48dfb6f25
angular-moon <xiaoxuss@gmail.com>
2024-05-17 13:58:46 +0800

    @vitejs/plugin-react-refresh -> @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
 
@@ -8,7 +8,7 @@ import checker from 'vite-plugin-checker';
 export default defineConfig({
   plugins: [
     svgr(),
-    reactRefresh(),
+    react(),
     checker({
       // use TypeScript check
       typescript: true,
